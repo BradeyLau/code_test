2e3d61742514a75faf0dbc297e18ac7ae44f113f
BradeyLau <zer0breathe5@gmail.com>
2024-09-29 13:31:04 +0800

    add an image

--- code240928/code240928/test.c
+++ code240928/code240928/test.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
+#include <string.h>
 
-int main() {
-	printf("hello world\n");
-
-	return 0;
+int main()
+{
+    char arr[] = { 'b', 'i', 't' };
+    char arr1[] = { 'b', 'i', 't' };
+    printf("%d\n", strlen(arr));
+    printf("%d\n", strlen(arr1));
+    return 0;
 }
+
+
+//int main() {
+//	printf("hello world\n");
+//
+//	return 0;
+//}
